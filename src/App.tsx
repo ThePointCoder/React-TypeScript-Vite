0b2b80de6fd305b6ab5b83404d395a8b6f0114a6
ThePointCoder <marta.janos82@gmail.com>
2021-04-28 15:34:54 +0200

    Add files via upload

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1><img src="./src/compass.svg"></img>React-Vite-TypeScript-Tailwind Weboldal</h1>
+        <h1><i class="fas fa-drafting-compass"></i>React-Vite-TypeScript-Tailwind Weboldal</h1>
         <button onClick={() => setCount((count) => count = "Hello Aranka!")}>{count}</button>
         <p>
           <a
